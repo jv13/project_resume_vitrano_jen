3c6274b3e3e3600ef9a89427f82b36f601985c63
Jen Vitrano <vitranojen@gmail.com>
2016-04-14 20:10:36 -0400

    Add comments to javascript file

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
+//jQuery plugin for background image
 $.backstretch( 'img/bkgrd.jpg' );
 
+//jQuery plugin for skills bar
 $( '.skillbar' ).skillBars( {
 } );
 
+//Event listener that listens for a mouseover on "reading" which will display new <p> text
 var reading = document.getElementById( 'reader' );
 reading.addEventListener( 'mouseover', function() {
     var myReading = document.createElement( 'p' );
@@ -10,6 +13,7 @@ reading.addEventListener( 'mouseover', function() {
 		document.getElementsByClassName( 'reading' )[ 0 ].appendChild( myReading );
 } );
 
+//Event listener that listens for a mouseover on "traveling" which will display new <p> text
 var traveling = document.getElementById( 'traveler' );
 traveling.addEventListener( 'mouseover', function() {
     var myTraveling = document.createElement( 'p' );
@@ -17,6 +21,7 @@ traveling.addEventListener( 'mouseover', function() {
 		document.getElementsByClassName( 'traveling' )[ 0 ].appendChild( myTraveling );
 } );
 
+//Event listener that listens for a mouseover on "frozen yogurt" which will display new <p> text
 var froyo = document.getElementById( 'frozen' );
 froyo.addEventListener( 'mouseover', function() {
     var myFroYo = document.createElement( 'p' );
@@ -24,27 +29,31 @@ froyo.addEventListener( 'mouseover', function() {
 		document.getElementsByClassName( 'froyo' )[ 0 ].appendChild( myFroYo );
 } );
 
+//Event listener that listens for a mouseover on "movies" which will display new <p> text
 var movie = document.getElementById( 'movies' );
 movie.addEventListener( 'mouseover', function() {
     var myMovie = document.createElement( 'p' );
-		myMovie.innerHTML = '<p>My top three favorite movies are: Up, Twister, and any Harry Potter movie.</p>';
+    myMovie.innerHTML = '<p>My top three favorite movies are: Up, Twister, and any Harry Potter movie.</p>';
 		document.getElementsByClassName( 'movies' )[ 0 ].appendChild( myMovie );
 } );
 
+//Event listener that listens for a mouseover on "tv shows" which will display new <p> text
 var tvshow = document.getElementById( 'tv' );
 tvshow.addEventListener( 'mouseover', function() {
     var myTv = document.createElement( 'p' );
-		myTv.innerHTML = '<p>My top three favorite TV shows are: Sons of Anarchy, 24, and Prison Break.</p>';
-		document.getElementsByClassName( 'shows' )[ 0 ].appendChild( myTv );
+    myTv.innerHTML = '<p>My top three favorite TV shows are: Sons of Anarchy, 24, and Prison Break.</p>';
+    document.getElementsByClassName( 'shows' )[ 0 ].appendChild( myTv );
 } );
 
+//Event listener that listens for a mouseover on "country music" which will display new <p> text
 var music = document.getElementById( 'music' );
 music.addEventListener( 'mouseover', function() {
     var myMusic = document.createElement( 'p' );
-		myMusic.innerHTML = '<p>I have been to more than 10 country concerts.</p>';
-		document.getElementsByClassName( 'country' )[ 0 ].appendChild( myMusic );
+    myMusic.innerHTML = '<p>I have been to more than 10 country concerts.</p>';
+    document.getElementsByClassName( 'country' )[ 0 ].appendChild( myMusic );
 } );
 
+//Event listener that listens for a mouseover on "running" which will display new <p> text
 var run = document.getElementById( 'run' );
 run.addEventListener( 'mouseover', function() {
     var myRun = document.createElement( 'p' );
@@ -52,6 +61,7 @@ run.addEventListener( 'mouseover', function() {
 		document.getElementsByClassName( 'running' )[ 0 ].appendChild( myRun );
 } );
 
+//Event listener that listens for a mouseover on "puzzles" which will display new <p> text
 var puzzles = document.getElementById( 'puzzle' );
 puzzles.addEventListener( 'mouseover', function() {
     var myPuzzle = document.createElement( 'p' );
